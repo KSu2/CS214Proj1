abf259ad42a700b9105c3a16db38a4c4891c5b1a
KSu2 <user.kevinsu@gmail.com>
2023-02-13 19:48:27 -0500

    added memgrind.c

--- test.c
+++ test.c
@@ -2,12 +2,10 @@
 #include "mymalloc.h"
 
 int main() {
-	printf("Hello world \n");
 	void* p = malloc(sizeof(int));
-	printf("This is a pointer created from mymalloc() %p\n",p);
-	printf("value of pointer created from mymalloc() %d\n", *((int *)p));
-	printf("value of next byte from pointer created from mymalloc() %d\n", *(((int *) p) + 1));
-	printf("value of next byte from pointer created from mymalloc() %d\n", *(((int *) p) + 2));
+	
+
+	
 	free(p);
 	int a = 1;
 	void* p2 = &a;
