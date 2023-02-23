1bf5405f3adbb0bff2186904d2b0b7fc7ba62c64
ghautham-sam01 <49998186+ghautham-sam01@users.noreply.github.com>
2023-02-23 16:19:30 -0500

    updated Test.c

--- test.c
+++ test.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include "mymalloc.h"
 
-//check basic free case
-void test_one(){
-	int testOne = 23;
-	void * a = &testOne;
-	free(a);
-}
-
 //check free for pointer not at beginning of chunk
-void test_two(){
+void test_one(){
     int * testTwo = (int*) malloc(sizeof(int)*100);
     free(testTwo+10);
 }
-
 //check double free
-void test_three(){
+void test_two(){
     int *testThree = (int*) malloc(100);
     free(testThree);
     free(testThree);
 }
 
-// Valid free
+//check basic free case
+void test_three(){
+	int testOne = 23;
+	void * a = &testOne;
+	free(a);
+}
+
+//check allocating more than 4069 bytes
 void test_four(){
+    void * t = malloc(5000);
+}
+
+
+// Valid free
+void test_five(){
     void * org = malloc(200);
     free(org);
     int * copy = org;
@@ -36,32 +41,17 @@ void test_four(){
     }
 }
 
-//check allocating more than 4069 bytes
-void test_five(){
-    void * t = malloc(5000);
-}
 
 int main() {
-	void* p = malloc(sizeof(int));
-	
-	free(p);
-	int a = 1;
-	void* p2 = &a;
-	free(p2);
-	// Performace tests
-	//Free mem that isnt malloc()
+
 	test_one();
 	
-	//Free the mem at an address that isnt returned by malloc()
 	test_two();
 	
-	//Free the same address a second time
 	test_three();
 		
-	//Run a valid test to see if our malloc() and free() works
 	test_four();
 
-	//Too much mem
 	test_five();
 
 	return 0;
