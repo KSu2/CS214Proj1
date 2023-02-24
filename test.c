fb224411b9803f4d3cd44632ce46132b12bc8fae
Kevin Su <44729913+KSu2@users.noreply.github.com>
2023-02-24 00:17:09 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
 #include "mymalloc.h"
 
-//check free for pointer not at beginning of chunk
-void test_one(){
-    int * testTwo = (int*) malloc(sizeof(int)*100);
-    free(testTwo+10);
+void test1(){
+	void * p1 = malloc(200);
+	free(p1);
+	void * p2 = malloc(200);
+	if(p1 == p2){
+		printf("SUCCESS! pointer is successfully created and freed");
+	}
+	else{
+		printf("FAILURE! free() did not work as intended");
+	}
 }
 //check double free
-void test_two(){
-    int *testThree = (int*) malloc(100);
-    free(testThree);
-    free(testThree);
+void test2(){
+	void * t = malloc(5000);
+	if(!t){
+		printf("SUCCESS! uninitialized memory did not allocate memory when size > 4087");
+	}
+	else{
+		printf("FAILURE! uninitialized memory allocated memory when it was not supposed to");
+	}
 }
 
-//check basic free case
-void test_three(){
-        int testOne = 23;
-        void * a = &testOne;
-        free(a);
+void test3(){
+        void * t = malloc(4087);
+        void * p = malloc(1);
+        if(!p){
+		printf("SUCCESS! didn't allocate memory when initialized memory was full");
+	}
+	else{
+		printf("FAILURE! allocated memory when it wasn't supposed to");
+	}
 }
 
-//check allocating more than 4069 bytes
-void test_four(){
-    void * t = malloc(5000);
+void test4(){
+	void * p1 = malloc(100);
+	void * p2 = malloc(100);
+	void * p3 = malloc(100);
+	free(p2);
+	p2 = malloc(100);
 }
-
 
 int main(int argc, char **argv){
         int test = argc > 1 ? atoi(argv[1]) : 0;
